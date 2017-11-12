b7e4c71cd9fb16d52d89c1bc6ba7f5a1e5292496
Sharmina Akhtar <sakht318@gmail.com>
2017-11-12 09:53:40 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
-  if array.length(1)
+  if array.length = 1
     array.join
-  elsif array.length(2)
+  elsif array.length == 2
     array.join(' and ')
   else
     array[-1] = "and " + array[-1]
