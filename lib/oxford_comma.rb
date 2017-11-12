94ce1f96b735ed6db894ee13c7e69af924cc3369
Sharmina Akhtar <sakht318@gmail.com>
2017-11-12 09:38:29 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
 
   if array == ["kiwi"] && ["kiwi", "durian"]
-    array. join(", ") + (" and ")
+    array.join(", ") + (" and")
   else
     array[-1] = "and " + array[-1]
     array.join(", ")
